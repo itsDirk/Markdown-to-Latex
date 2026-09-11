71797da72030f069e3d6153b4ba45b8f77f17bef
Dirk <vanroosmalendirk@gmail.com>
2026-09-11 20:20:38 +0200

    Fix math blocks treated as math lines

--- src/components/mathblocks.js
+++ src/components/mathblocks.js
@@ -1,13 +1,4 @@
 export function removeMathBlocks(content) {
-    // Store and replace $math$ with =MATH=LINE=0= temporarily
-    let mathLines = [];
-    content = content.replace(/\$.*?\$/g, match => {
-        const id = mathLines.length;
-        const code = match.slice(1, -1);
-        mathLines.push(`\$${code}\$`);
-        return `=MATH=LINE=${id}=`;
-    });
-
     // Store and replace $$math$$ with =MATH=BLOCK=0= temporarily
     let mathBlocks = [];
     content = content.replace(/\$\$.*?\$\$/gs, match => {
@@ -17,18 +8,27 @@ export function removeMathBlocks(content) {
         return `=MATH=BLOCK=${id}=`;
     });
 
+    // Store and replace $math$ with =MATH=LINE=0= temporarily
+    let mathLines = [];
+    content = content.replace(/\$.*?\$/g, match => {
+        const id = mathLines.length;
+        const code = match.slice(1, -1);
+        mathLines.push(`\$${code}\$`);
+        return `=MATH=LINE=${id}=`;
+    });
+
     return {content, mathLines, mathBlocks};
 }
 
 export function restoreMathBlocks(content, mathLines, mathBlocks) {
-    // Replace =MATH=BLOCK=0= with the original string
-    content = content.replace(/=MATH=BLOCK=(\d+)=/g, (_, id) => {
-        return mathBlocks[id];
-    });
-
     // Replace =MATH=LINE=0= with the original string
     content = content.replace(/=MATH=LINE=(\d+)=/g, (_, id) => {
         return mathLines[id];
+    });
+
+    // Replace =MATH=BLOCK=0= with the original string
+    content = content.replace(/=MATH=BLOCK=(\d+)=/g, (_, id) => {
+        return mathBlocks[id];
     });
 
     return content;
